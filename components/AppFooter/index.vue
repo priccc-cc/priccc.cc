9bb01ca5177e366cb2df421525eae107ae98b82a
Priccc <zleipriccc@gmail.com>
2024-01-03 18:41:16 +0800

    add filing

--- components/AppFooter/index.vue
+++ components/AppFooter/index.vue
@@ -2,12 +2,18 @@
   <div class="app-footer-component">
     <div class="filiing-block">
       <el-space>
+        <el-link
+          class="psf"
+          href="https://beian.mps.gov.cn/#/query/webSearch?code=11010802043767"
+          ref="noreferrer"
+          target="_blank"
+        >
+          <img src="~/assets/images/filing.png" alt="" style="width: 16px" />
+          <span>京公网安备11010802043767号</span>
+        </el-link>
         <el-link class="icp" href="https://beian.miit.gov.cn/" target="_blank">
           <span>京ICP备2024041203号</span>
         </el-link>
-        <!-- <el-link class="psf" href="https://beian.miit.gov.cn/" target="_blank">
-          <span>icp 公安备案</span>
-        </el-link> -->
       </el-space>
     </div>
   </div>
